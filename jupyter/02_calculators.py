739466b1404e798ee869b9d1fdaff63ae63614c1
Levente Nagy <lnagy@chemaxon.com>
2026-08-03 13:33:25 +0200

    COG-815 Add MSA 3D examples

--- jupyter/02_calculators.py
+++ jupyter/02_calculators.py
@@ -334,4 +334,21 @@ for hmo_val in nucl_ord:
 print()
 print('Pi energy: '+ str(pi_energy))
 
+# %% [markdown]
+# ### **Molecular Surface Area 3D**
+
+# %% [markdown]
+# There are two versions available for 3D molecular surface area calculation:
+# - [van der Waals molecular surface area](https://docs.chemaxon.com/latest/calculators_molecular-surface-area-plugin-3d.html)
+# - [solvent accessible molecular surface area](https://docs.chemaxon.com/latest/calculators_molecular-surface-area-plugin-3d.html)
+
 # %%
+from chemaxon.io import import_mol
+from chemaxon.calculations import van_der_waals_surface_area, solvent_accessible_surface_area
+
+aspirin = import_mol('CC(=O)OC1=CC=CC=C1C(O)=O')
+vdw_result = van_der_waals_surface_area(aspirin)
+asa_result = solvent_accessible_surface_area(aspirin)
+
+print('van der Waals surface area of aspirin: ' + str(vdw_result.surface_area))
+print('solvent accessible surface area of aspirin: ' + str(asa_result.surface_area))
